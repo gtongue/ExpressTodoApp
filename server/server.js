511ac156c70d7af398fb26d0877eb08dfcee15eb
Garrett Tongue <garrettjtongue@gmail.com>
2018-01-23 23:17:48 -0800

    Add models

--- server/server.js
+++ server/server.js
@@ -1,45 +1,10 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+let { mongoose } = require('./db/mongoose');
+let { Todo } = require("./models/todo");
+let { User } = require("./models/user");
 
-let Todo = mongoose.model("Todo", {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
-});
-
-let newTodo = new Todo({
-    text: "Meme"
-});
-
-// newTodo.save().then((doc) => {
-//     console.log(doc);
-// }, (err) => {
-//     console.error(err);
-// });
-
-let User = mongoose.model("User", {
-    email: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: 1
-    }
-});
-
-let newUser = new User({
-    email: "memer@memer.meme"
-});
-newUser.save().then((doc) => console.log(doc), (err) => console.error(err));
+let app = express();
+let port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Server started on port ${port}`));
