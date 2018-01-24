df9214fde6bec062c9068e5bd7ca066e6af16880
Garrett Tongue <garrettjtongue@gmail.com>
2018-01-24 09:29:40 -0800

    Start adding tests

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
-
+mongoose.connect("mongodb://localhost:27017/TodoApp").then((result) => {
+  console.log("Connected to mongo database");
+}, (err) => {
+  console.log("Problem connecting to mongodb database");
+});
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -6,5 +6,18 @@ let { Todo } = require("./models/todo");
 let { User } = require("./models/user");
 
 let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo(req.body);
+  todo.save().then((doc) => res.send(doc),(err) => {
+    res.statusCode = 400;
+    res.send(err.message);
+  });
+});
+
 let port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`Server started on port ${port}`));
+
+module.exports  = {app};
